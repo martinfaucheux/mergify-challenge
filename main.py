7b181fc1572b920c60ef6642a6ee7d2bafb7e33e
Martin Faucheux <martin.faucheux@gmail.com>
2025-12-15 17:32:24 +0100

    add dotenv lib

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+load_dotenv()
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/repos/{github_user}/{github_repo}/starneighbours")
 async def root():
     return {"message": "Hello World"}
